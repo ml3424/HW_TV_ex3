4f55ff761e4af877029cafecce2b233bbfff0f8c
ml3424 <ml3424@bs.amalnet.k12.il>
2023-12-19 09:34:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hw_tv_ex3/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hw_tv_ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView tV;
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+    }
+
+}
